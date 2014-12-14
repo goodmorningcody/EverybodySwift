d2e8b378c3ff9cf48913d68efaa53a00a5d7baaa
Cody <cody.yun@daumkakao.com>
2014-12-14 17:52:25 +0900

    UILabel 완료

---
+++ UIKitSample/UIKitSample/LabelViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LabelViewController.swift
+//  UIKitSample
+//
+//  Created by Cody on 2014. 12. 14..
+//  Copyright (c) 2014년 tiekle. All rights reserved.
+//
+
+import UIKit
+
+class LabelViewController: UIViewController {
+
+    @IBOutlet var label : UILabel? = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label!.text = "Hello Swift"
+        label!.frame.origin.x = UIScreen.mainScreen().bounds.width/2-label!.frame.width/2
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        label!.frame.origin.x = UIScreen.mainScreen().bounds.width/2-label!.frame.width/2
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
