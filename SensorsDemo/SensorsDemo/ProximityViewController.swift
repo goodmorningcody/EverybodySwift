ba63881db9ba54b4d9434c265baf66717a8783d5
Cody <goodmorningcody@gmail.com>
2015-01-05 22:02:50 +0900

    Created SensorsDemo project

---
+++ SensorsDemo/SensorsDemo/ProximityViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProximityViewController.swift
+//  SensorsDemo
+//
+//  Created by Cody on 2015. 1. 5..
+//  Copyright (c) 2015년 tiekle. All rights reserved.
+//
+
+import UIKit
+
+class ProximityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
