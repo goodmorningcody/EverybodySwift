e2194a5a72ce3009912c1282692f2d3cae2433c9
Cody <goodmorningcody@gmail.com>
2015-01-08 02:26:54 +0900

    Gyroscope 센서

--- SensorsDemo/SensorsDemo/GyroscopeViewController.swift
+++ SensorsDemo/SensorsDemo/GyroscopeViewController.swift
@@ -7,29 +7,41 @@
 //
 
 import UIKit
+import CoreMotion
 
 class GyroscopeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        var motionManager = CMMotionManager()
+        if motionManager.gyroAvailable==false {
+            println("Gyroscopre 센서를 사용할 수 없습니다.")
+        }
+        else {
+            motionManager.gyroUpdateInterval = 0.2
+            motionManager.startGyroUpdatesToQueue(NSOperationQueue.currentQueue(), withHandler: {(gyroData: CMGyroData!, error: NSError!) in
+                if error != nil {
+                    println("x : "+gyroData.rotationRate.x.description)
+                    println("y : "+gyroData.rotationRate.y.description)
+                    println("z : "+gyroData.rotationRate.z.description)
+                }
+            })
+        }
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "orientationChanged:", name: UIDeviceOrientationDidChangeNotification, object: nil)
+    }
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        NSNotificationCenter.defaultCenter().removeObserver(self)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func orientationChanged(notification : NSNotification) {
+        println(UIDevice.currentDevice().orientation.rawValue)
     }
-    */
-
 }
