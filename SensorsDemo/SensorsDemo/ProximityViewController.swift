abd8ca06cb1b10be026836b631255c8803f4db55
Cody <goodmorningcody@gmail.com>
2015-01-10 16:02:59 +0900

    근접센서

--- SensorsDemo/SensorsDemo/ProximityViewController.swift
+++ SensorsDemo/SensorsDemo/ProximityViewController.swift
@@ -12,8 +12,23 @@ class ProximityViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIDevice.currentDevice().proximityMonitoringEnabled = true
+        if UIDevice.currentDevice().proximityMonitoringEnabled==false {
+            println("근접센서를 사용할 수 없습니다.")
+        }
+        else {
+            NSNotificationCenter.defaultCenter().addObserver(self, selector:"proximityChanged:", name:UIDeviceProximityStateDidChangeNotification, object: nil)
+        }
     }
-
+    func proximityChanged(notification: NSNotification) {
+        if UIDevice.currentDevice().proximityState==true {
+            println("proximityChanged - 근접센서가 무언가에 의해 가려졌습니다.")
+        }
+        else {
+            println("proximityChanged - 근접센서를 가린 오브젝트가 없습니다.")
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
